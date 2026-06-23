e6ab04c2fbcc537024e98a7204daf16d03291956
SPFXUNLIMITED <33075671+SPFXUNLIMITED@users.noreply.github.com>
2026-06-23 11:47:06 -0700

    Update config.php

--- config.php
+++ config.php
@@ -1,4 +1,17 @@
 <?php
-// Google reCAPTCHA v2
-define('RECAPTCHA_SITE_KEY',   getenv('RECAPTCHA_SITE_KEY')   ?: '6LdUs-csAAAAAO0OwhwPWMTV941Vs7jN3XWB7MhT');
-define('RECAPTCHA_SECRET_KEY', getenv('RECAPTCHA_SECRET_KEY') ?: '6LdUs-csAAAAAC1ezjVMiAAUtS0GWoQrvYSsITCo');
+// config.php
+
+return [
+  'version' => '1.0.0',
+  'db' => [
+    'host' => '127.0.0.1',
+    'name' => 'spfx_ghostlaser',
+    'user' => 'spfx_ghost',
+    'pass' => 'Beverly90210##',
+    'charset' => 'utf8mb4',
+  ],
+  'recaptcha' => [
+    'site_key'   => '6LdUs-csAAAAAO0OwhwPWMTV941Vs7jN3XWB7MhT',
+    'secret_key' => '6LdUs-csAAAAAC1ezjVMiAAUtS0GWoQrvYSsITCo',
+  ],
+];
