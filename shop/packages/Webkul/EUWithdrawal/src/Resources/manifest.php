ba6bf6832127d996025c976442590dd6b0e0ec83
SPFXUNLIMITED <33075671+SPFXUNLIMITED@users.noreply.github.com>
2026-09-24 04:06:46 -0700

    Delete shop directory

--- shop/packages/Webkul/EUWithdrawal/src/Resources/manifest.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return [
-    'name' => 'Webkul Bagisto EUWithdrawal',
-    'version' => core()->version(),
-];
--- shop/packages/Webkul/GhostLaser/src/Routes/shop-routes.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Webkul\GhostLaser\Http\Controllers\Shop\GhostLaserController;
-
-Route::group(['middleware' => ['web', 'theme', 'locale', 'currency'], 'prefix' => 'ghostlaser'], function () {
-    Route::get('', [GhostLaserController::class, 'index'])->name('shop.ghostlaser.index');
-});
